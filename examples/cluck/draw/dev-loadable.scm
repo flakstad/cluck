4a288858ec8ae0c9a6509f42a29c77c7aedd9396
Andreas Flakstad <andreas@breyta.ai>
2026-04-10 15:31:23 +0200

    Make draw new clear canvas visibly

--- examples/cluck/draw/dev-loadable.scm
+++ examples/cluck/draw/dev-loadable.scm
@@ -10,6 +10,6 @@
 
 (include "src/cluck.scm")
 
-(hash-table-set! *cluck-loaded-namespaces* 'cluck.examples.draw.sdl3 #t)
-
 (include "examples/cluck/draw/src/sdl3.clk")
+
+(hash-table-set! *cluck-loaded-namespaces* 'cluck.examples.draw.sdl3 #t)
